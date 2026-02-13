a9f3990b52dc2e67c146366d8a4e607cb32f12de
ASUS_ <ratsimbaprincy@gmail.com>
2026-02-13 17:25:46 +0300

    ajout modele Book

---
+++ backend/src/models/Book.js
@@ -0,0 +1,12 @@
+// models/Book.js
+import mongoose from 'mongoose';
+
+const BookSchema = new mongoose.Schema({
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
+});
+
+export default mongoose.model('Book', BookSchema);
